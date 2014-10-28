71755524b6a30d08e47bcd62d3d1fafe958dfcd6
asamsonov <samsonov68rus@gmail.com>
2014-10-28 20:21:20 +0400

    interface

--- src/TestApplications/Controllers/AuthController.java
+++ src/TestApplications/Controllers/AuthController.java
@@ -9,8 +9,20 @@ import TestApplications.Views.SingUpView;
 import TestApplications.Views.AuthView;
 import TestApplications.Workers.JSONWorker;
 import TestApplications.Views.EnterView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -18,28 +30,31 @@ import org.json.simple.JSONObject;
  *
  * @author artemsamsonov
  */
-public class AuthController {
+public class AuthController implements WindowListener, ListSelectionListener, ActionListener, 
+        KeyListener, MouseListener {
+    
     private final AuthView view;
     private final JSONArray usersArray;
+    
+    private int index = -1;
     
     public AuthController (AuthView view) throws Exception {
         this.view = view;
         this.usersArray = JSONWorker.open("users/users.json");
-        for (int i = 0; i < usersArray.size(); i++) {
-            JSONObject tmp = (JSONObject) usersArray.get(i);
-            this.view.chooseUserComboBox.addItem(tmp.get("name"));
+
+        DefaultListModel listmodel = new DefaultListModel();
+        for (Object i : this.usersArray) {
+            listmodel.addElement(((JSONObject)i).get("lastName") 
+                    + "\t" + ((JSONObject)i).get("name") + "\t"+((JSONObject)i).get("firstName"));
         }
-        if (usersArray.size() == 0) {
-            this.view.enterUserButton.setEnabled(false);
-        }
-        if (this.view.chooseUserComboBox.getItemCount() != 0) {
-            this.view.chooseUserComboBox.setSelectedIndex(this.view.chooseUserComboBox.getItemCount()-1);
-        }
+        this.view.usersList.setModel(listmodel);
+        this.view.usersList.setSelectedIndex(-1);
+        
     }
     public void enter() {
-        this.view.setVisible(false);
         try {
-            new EnterView(JSONWorker.get(usersArray, this.view.chooseUserComboBox.getSelectedIndex())).setVisible(true);
+            new EnterView(JSONWorker.get(usersArray, this.index)).setVisible(true);
+            this.view.setVisible(false);
         } catch (Exception ex) {
             Logger.getLogger(AuthView.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -47,5 +62,121 @@ public class AuthController {
     public void create() throws Exception {
         this.view.setVisible(false);
         new SingUpView().setVisible(true);
+    }
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+        
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+        if (this.usersArray.isEmpty()) {
+            this.view.enterUserButton.setEnabled(false);
+        } else {
+            this.view.enterUserButton.setEnabled(true);
+        }
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        this.index = ((JList)e.getSource()).getSelectedIndex();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Новый пользователь")) {
+            try {
+                this.create();
+            } catch (Exception ex) {
+                Logger.getLogger(AuthController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if (e.getActionCommand().equals("Войти")) {
+            if (this.isIndexRight()) {
+                this.enter();
+            }
+        }
+        if (e.getActionCommand().equals("Выход")) {
+            this.view.setVisible(false);
+            this.view.dispose();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER && this.isIndexRight()) {
+            this.enter();
+        }
+    }
+    
+    private boolean isIndexRight() {
+        if (this.index >= 0 && this.index < this.usersArray.size()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getClickCount() == 2) {
+            this.enter();
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
--- src/TestApplications/Controllers/SingUpController.java
+++ src/TestApplications/Controllers/SingUpController.java
@@ -10,7 +10,16 @@ import TestApplications.Views.SingUpView;
 import TestApplications.Workers.FileWorker;
 import TestApplications.Workers.JSONWorker;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.InputMethodListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.BorderFactory;
+import javax.swing.JTextField;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -18,7 +27,7 @@ import org.json.simple.JSONObject;
  *
  * @author artemsamsonov
  */
-public class SingUpController {
+public class SingUpController implements KeyListener, ActionListener, InputMethodListener {
     private final SingUpView view;
     final private int countOfTests;
     
@@ -59,30 +68,76 @@ public class SingUpController {
         String midname = this.view.textUserSurname.getText();
         String type = this.view.textUserType.getText();
         
-        if (name.isEmpty()) {
-            this.view.textUserName.setBorder(BorderFactory.createLineBorder(Color.red));
-            return false;
-        } else {
-            this.view.textUserName.setBorder(null);
-        }
         if (lastname.isEmpty()) {
             this.view.textUserLastname.setBorder(BorderFactory.createLineBorder(Color.red));
             return false;
         } else {
-            this.view.textUserLastname.setBorder(null);
+            this.view.textUserLastname.setBorder(BorderFactory.createLineBorder(Color.gray));
+        }
+        if (name.isEmpty()) {
+            this.view.textUserName.setBorder(BorderFactory.createLineBorder(Color.red));
+            return false;
+        } else {
+            this.view.textUserName.setBorder(BorderFactory.createLineBorder(Color.gray));
         }
         if (midname.isEmpty()) {
             this.view.textUserSurname.setBorder(BorderFactory.createLineBorder(Color.red));
             return false;
         } else {
-            this.view.textUserSurname.setBorder(null);
+            this.view.textUserSurname.setBorder(BorderFactory.createLineBorder(Color.gray));
         }
         if (type.isEmpty()) {
             this.view.textUserType.setBorder(BorderFactory.createLineBorder(Color.red));
             return false;
         } else {
-            this.view.textUserType.setBorder(null);
+            this.view.textUserType.setBorder(BorderFactory.createLineBorder(Color.gray));
         }
         return true;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+          
+                try {
+                    this.apply();
+                } catch (Exception ex) {
+                    Logger.getLogger(SingUpController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            this.apply();
+        } catch (Exception ex) {
+            Logger.getLogger(SingUpController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void inputMethodTextChanged(InputMethodEvent event) {
+        if (Character.isDigit(event.getText().current()) ||
+                Character.isWhitespace(event.getText().current())) {
+            event.consume();
+        } else {
+        }
+    }
+
+    @Override
+    public void caretPositionChanged(InputMethodEvent event) {
+        
     }
 }
